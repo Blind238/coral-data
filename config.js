e5c16a3d4f14e6bd62802db3091452c749aa775f
Jeremy Granadillo <akakitsu@gmail.com>
2018-12-18 11:31:25 -0400

    use config file for directory and id class names

---
+++ config.js
@@ -0,0 +1,13 @@
+"use strict";
+const config = {
+  idClasses: [
+    'coral',
+    'seagrass',
+    'sand'
+  ],
+  srcDir: 'images/pending/',
+  destDir: 'images/zipped/',
+  zipDir: 'images/zips/',
+}
+
+module.exports = config;
