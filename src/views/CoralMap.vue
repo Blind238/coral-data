b94d0229e2923eb68bc5dbf138f2d204d2ca6356
Jeremy Granadillo <akakitsu@gmail.com>
2019-01-16 14:59:05 -0400

    sassify CoralMap styles

--- src/views/CoralMap.vue
+++ src/views/CoralMap.vue
@@ -32,49 +32,46 @@ export default {
 
 <style lang="scss" scoped>
 .coral-map-container {
-  position:relative;
+  position: relative;
 }
+
 .coral-map {
   width: 100vw;
   height: calc(100vh - 78px);
+
+  &-legend, &-control {
+    position: absolute;
+    z-index: 1001;
+  }
+
+  &-legend {
+    top: 0;
+    left: 0;
+    margin: 1.5em 2em;
+    text-align: left;
+
+    ul { list-style: none; }
+    li { margin: 0.5em; }
+    span {
+      width: 1em;
+      height: 1em;
+      float: left;
+      margin-right: 0.5em;
+    }
+
+    .coral { background-color: #f7403a; }
+    .seagrass { background-color: #00ad00; }
+    .sand { background-color: #fcda90; }
+    .other { background-color: #afafaf; }
+  }
+
+  &-control {
+    top: 0;
+    right: 0;
+    margin: 1.5em;
+    margin-right: 4em;
+  }
 }
 
-.coral-map-legend, .coral-map-control {
-  position: absolute;
-  z-index: 1001;
-}
-
-.coral-map-legend {
-  top: 0;
-  left: 0;
-  margin: 2em;
-}
-
-.coral-map-legend ul {
-  list-style: none;
-}
-
-.coral-map-legend li {
-  margin: 0.5em;
-}
-
-.coral-map-legend span {
-  width: 1em;
-  height: 1em;
-  float: left;
-  margin-right: 0.5em;
-}
-
-.coral-map-legend .coral {background-color: #f7403a;}
-.coral-map-legend .seagrass {background-color: #00ad00;}
-.coral-map-legend .sand {background-color: #fcda90;}
-.coral-map-legend .other {background-color: #afafaf;}
-
-.coral-map-control {
-  top: 0;
-  right: 0;
-  margin: 2em;
-}
-
-@import url('~leaflet/dist/leaflet.css');
+@import '~leaflet/dist/leaflet.css';
 </style>
