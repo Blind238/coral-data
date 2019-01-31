1f2f8f0d79f6df1350a8753ca094ec17b69afd68
Jeremy Granadillo <akakitsu@gmail.com>
2019-01-31 13:46:44 -0400

    Swap theme icons and adjust colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
-import colors from 'vuetify/es5/util/colors'
+// import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    // primary: '#00B0FF',
+    primary: '#00B0FF',
     // primary: colors.cyan.darken2,
-    primary: colors.blue.lighten1,
+    // primary: colors.blue.lighten1,
+    secondary: '#FF193B',
     // secondary: '#FFFF8D',
     // secondary: '#FFC400',
     // secondary: '#EC407A',
     // secondary: '#FF80AB',
     // secondary: colors.yellow.lighten1,
-    secondary: colors.pink.lighten1,
+    // secondary: colors.pink.lighten1,
+    accent: '#FFF400',
     // accent: '#82B1FF',
     // accent: '#FF80AB',
     // accent: '#283593',
@@ -24,8 +26,9 @@ Vue.use(Vuetify, {
     warning: '#FFC107'
   },
   customProperties: true,
-  iconfont: 'mdi'
+  iconfont: 'md'
 })
 // #00B0FF, #FFFF8D, #FF80AB
 // #00B0FF, #FFC400, #283593
 // #00B0FF, #EC407A, #388E3C
+// #00B0FF, #FFF400, #FF193B
